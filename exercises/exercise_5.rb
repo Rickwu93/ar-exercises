a9f32b2ed2a3ba8b835ff598ecc118c24766ce24
Rick <rickywu209@gmail.com>
2022-05-03 04:11:39 +0000

    exercise 1-7 tested

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,10 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@totalsum = Store.sum(:annual_revenue)
+@avg = Store.average(:annual_revenue)
+@overmill = Store.where("annual_revenue > 1000000").count
+
+puts @totalsum
+puts @avg
+puts @overmill
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,8 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 48)
+@store1.employees.create(first_name: "Ricky", last_name: "Wu", hourly_rate: 160)
+@store1.employees.create(first_name: "Alvin", last_name: "Tacos", hourly_rate: 80)
+@store2.employees.create(first_name: "Kevin", last_name: "Lang", hourly_rate: 40)
+@store2.employees.create(first_name: "Tony", last_name: "You", hourly_rate: 50)
